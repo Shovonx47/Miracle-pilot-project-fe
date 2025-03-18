b3b8902f1c14d7a3e2b335a0b79cc510136b03f3
farukphero <farukk6319@gmail.com>
2025-03-18 16:29:06 +0600

    populate attendance

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['i.ibb.co', 'example.com', 'res.cloudinary.com', 'images.unsplash.com'],
-  },
-};
-
-module.exports = nextConfig;
